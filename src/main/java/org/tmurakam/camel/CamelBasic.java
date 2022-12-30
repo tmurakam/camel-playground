c5e32e30e89dbac3a41841a684e7cd70df1b0f5d
Takuya Murakami <tmurakam@tmurakam.org>
2022-12-30 12:13:38 +0900

    add: threads / queue test

--- src/main/java/org/tmurakam/camel/CamelBasic.java
+++ src/main/java/org/tmurakam/camel/CamelBasic.java
@@ -2,6 +2,9 @@ package org.tmurakam.camel;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -18,7 +21,8 @@ public final class CamelBasic {
         CamelContext camel = new DefaultCamelContext();
 
         //camel.addRoutes(helloRoute());
-        camel.addRoutes(asyncExceptionRoute());
+        //camel.addRoutes(asyncExceptionRoute());
+        camel.addRoutes(threadsQueueTestRoute());
 
         // start is not blocking
         camel.start();
@@ -51,6 +55,42 @@ public final class CamelBasic {
                         .log("In async threads")
                         .throwException(new RuntimeException("ex1"))
                         .log("Should not reach here");
+            }
+        };
+    }
+
+    static RouteBuilder threadsQueueTestRoute() {
+        return new RouteBuilder() {
+            int counter = 0;
+            
+            @Override
+            public void configure() throws Exception {
+                from("timer:foo?period=10")
+                        .onException(Exception.class)
+                            .log("Handle exception")
+                            .handled(true)
+                        .end()
+
+                        .setExchangePattern(ExchangePattern.InOnly)
+                        .process(exchange -> {
+                            counter++;
+                            exchange.setExchangeId(Integer.toString(counter));
+                            log.info("Consume: exchange id = {}", exchange.getExchangeId());
+                        })
+
+                        // Create threads = 1, queue size = 10
+                        .threads()
+                        .poolSize(1)
+                        .maxPoolSize(1)
+                        .maxQueueSize(10)
+
+                        // Create slow processor. This will 'block' the input.
+                        .process(exchange -> {
+                            String id = exchange.getExchangeId();
+                            log.info("Start Processor: id = {}", id);
+                            Thread.sleep(3 * 1000);
+                            log.info("Finish Processor: id = {}", id);
+                        });
             }
         };
     }
